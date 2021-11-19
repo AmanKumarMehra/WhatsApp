1092bd45e762aab3792e4d7d33786c2ea9124310
Aman Kumar <amankumar34276@gmail.com>
2021-11-19 19:26:27 +0530

    I am building a clone of WhatsApp to learn how to add all of it's features.

---
+++ app/src/main/java/com/example/whatsapp/Calls_Fragment.java
@@ -0,0 +1,26 @@
+package com.example.whatsapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Calls_Fragment extends Fragment {
+
+
+    public Calls_Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_calls_, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/whatsapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.whatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+    }
+}
